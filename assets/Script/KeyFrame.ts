771dfc3bd63693bb83717eb474d059713ecbe50c
Skylark92 <skylark92@live.co.kr>
2024-01-25 22:12:00 +0900

    Cocos Callback

--- assets/Script/KeyFrame.ts
+++ assets/Script/KeyFrame.ts
@@ -7,15 +7,24 @@
 
 const { ccclass, property } = cc._decorator;
 
+let num = 0;
 @ccclass
 export default class NewClass extends cc.Component {
+  @property(cc.Label)
+  lblDesc: cc.Label = null;
+
   // LIFE-CYCLE CALLBACKS:
 
   onLoad() {
     const anim = this.getComponent(cc.Animation);
 
-    const animState = anim.play("scale");
+    const animState = anim.play("rotation");
     animState.wrapMode = cc.WrapMode.Loop;
+  }
+
+  OnScaleEnd() {
+    this.lblDesc.string = `${++num}`;
+    cc.log("scale animation end 2 ...");
   }
 
   // start () {
